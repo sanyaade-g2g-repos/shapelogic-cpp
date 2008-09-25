c22a80852769dbd343d945ee93cf1491587bc03a
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-09-25 06:11:55 +0000

    Added Util class.

---
+++ src/main/cpp/Util.h
@@ -0,0 +1,18 @@
+/*
+ * Util.h
+ *
+ *  Created on: Sep 25, 2008
+ *      Author: Sami Badawi
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+class Util {
+public:
+	Util();
+	virtual ~Util();
+	static const char * singleFileDialog();
+};
+
+#endif /* UTIL_H_ */

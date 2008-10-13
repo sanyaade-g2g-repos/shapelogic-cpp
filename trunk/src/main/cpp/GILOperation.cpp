073400c2b825806a64e3b3414cf10a2c5ff8e772
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-10-13 15:40:49 +0000

    First edge detector is working.

--- trunk/src/main/cpp/GILOperation.cpp
+++ trunk/src/main/cpp/GILOperation.cpp
@@ -31,6 +31,40 @@ struct halfdiff_cast_channels {
 
 template <typename SrcView, typename DstView>
 void x_gradient(const SrcView& src, const DstView& dst) {
+
+    int max_x = src.width()-1;
+    for (int y=0; y<src.height(); ++y) {
+        typename SrcView::x_iterator src_it = src.row_begin(y);
+        typename DstView::x_iterator dst_it = dst.row_begin(y);
+
+        for (int x=1; x < max_x; ++x) {
+            for (int c=0; c< 1; ++c) {
+//            for (int c=0; c< SrcView::num_channels; ++c) {
+            	dst_it[x][c] = (src_it[x+1][c] - src_it[x-1][c])/2;
+            }
+        }
+    }
+}
+
+void x_gradient8rgb(const boost::gil::rgb8_view_t& src, const boost::gil::rgb8_view_t& dst) {
+
+    int max_x = src.width()-1;
+    for (int y=0; y<src.height(); ++y) {
+    	boost::gil::rgb8_view_t::x_iterator src_it = src.row_begin(y);
+    	boost::gil::rgb8_view_t::x_iterator dst_it = dst.row_begin(y);
+
+        for (int x=1; x < max_x; ++x) {
+//            for (int c=0; c< 1; ++c) {
+//            for (int c=0; c< rgb8_view_t::num_channels; ++c) {
+            for (int c=0; c< num_channels<rgb8_view_t>::value; ++c) {
+            	dst_it[x][c] = (src_it[x+1][c] - src_it[x-1][c])/2;
+            }
+        }
+    }
+}
+
+template <typename SrcView, typename DstView>
+void x_gradient_generic(const SrcView& src, const DstView& dst) {
     typedef typename channel_type<DstView>::type dst_channel_t;
 
     for (int y=0; y<src.height(); ++y) {
@@ -51,15 +85,19 @@ void x_luminosity_gradient(const SrcView& src, const DstView& dst) {
 }
 
 
-rgb8_view_t make_rgb8_view_t(Fl_Image * input) {
-	rgb8_view_t view = interleaved_view(input->w(),input->h(),(rgb8_view_t::value_type *)input->data(),input->w()*3);
+rgb8_view_t GILOperation::make_rgb8_view_t(Fl_Image * input) {
+	rgb8_view_t view = interleaved_view(input->w(),input->h(),(rgb8_view_t::value_type *)(*input->data()),input->w()*3);
 	return view;
 }
 
+int getFirstByteInImage(rgb8_view_t & view) {
+	rgb8_pixel_t* data = view.row_begin(0);
+	int number = data[0][0];
+	return number;
+}
+
 void printFirst(rgb8_view_t & view) {
-	boost::gil::rgb8_pixel_t* data = view.row_begin(0);
-	int number = (*data)[0];
-	cerr << " first byte " << number << endl;
+	cerr << " first byte " << getFirstByteInImage(view) << endl;
 }
 
 void GILOperation::fltkGradient(Fl_Image * input, Fl_Image * output) {
@@ -68,8 +106,8 @@ void GILOperation::fltkGradient(Fl_Image * input, Fl_Image * output) {
 	rgb8_view_t outputView = make_rgb8_view_t(output);
 	cerr << "Input ";
 	printFirst(inputView);
-	x_gradient(inputView, outputView);
-	x_gradient(make_rgb8_view_t(input),make_rgb8_view_t(output));
+	x_gradient8rgb(inputView, outputView);
+//	x_gradient8rgb(make_rgb8_view_t(input),make_rgb8_view_t(output));
 	cerr << "Output ";
 	printFirst(outputView);
 }

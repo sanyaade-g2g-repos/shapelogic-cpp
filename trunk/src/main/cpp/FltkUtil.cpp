351efd780305b66c0d72e07d3d6209dbfda032f9
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-10-22 21:30:49 +0000

    Added type to file dialog. Took out unused constructor.

--- trunk/src/main/cpp/FltkUtil.cpp
+++ trunk/src/main/cpp/FltkUtil.cpp
@@ -12,28 +12,23 @@
 static std::string filename("");
 static std::string directory(".");
 
-const char * FltkUtil::singleFileDialog() {
-	  Fl_File_Chooser chooser(directory.c_str(),			// directory
-			    "*",			// filter
-			    Fl_File_Chooser::SINGLE, 	// chooser type
-			    "ShapeLogic file chooser");	// title
-	  chooser.show();
+const char * FltkUtil::singleFileDialog(bool onlyExisting) {
+	int dialogType = Fl_File_Chooser::SINGLE;
+	if (!onlyExisting)
+		dialogType = Fl_File_Chooser::CREATE;
+	Fl_File_Chooser chooser(directory.c_str(),			// directory
+			"*",			// filter
+			dialogType, 	// chooser type
+			"ShapeLogic file chooser");	// title
+	chooser.show();
 
 	// Block until user picks something.
 	//     (The other way to do this is to use a callback())
 	//
-	  while(chooser.shown())
-	  { Fl::wait(); }
-	  directory = chooser.directory();
-	  filename = chooser.value();
-	  return filename.c_str();
-}
-
-FltkUtil::FltkUtil() {
-	// TODO Auto-generated constructor stub
-
-}
-
-FltkUtil::~FltkUtil() {
-	// TODO Auto-generated destructor stub
+	while(chooser.shown()) {
+		Fl::wait();
+	}
+	directory = chooser.directory();
+	filename = chooser.value();
+	return filename.c_str();
 }

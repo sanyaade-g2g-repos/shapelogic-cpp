7a0fb5488a59249aab4d50b3d8830e4a3b25221a
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-10-23 02:31:43 +0000

    Return true if file save was successful.

--- trunk/src/main/cpp/GILOperation.h
+++ trunk/src/main/cpp/GILOperation.h
@@ -21,8 +21,8 @@ public:
 	static void fltkSobelXY(Fl_Image * input, Fl_Image * output);
 	static void xLuminosityGradient(const char * inputFile, const char * outputFile);
 	static boost::gil::rgb8_view_t make_rgb8_view_t(Fl_Image * input);
-	static void saveJpg(const char * filename, Fl_Image * input);
-	static void saveAnyImage(const char * filename, Fl_Image * input);
+	static bool saveJpg(const char * filename, Fl_Image * input);
+	static bool saveAnyImage(const char * filename, Fl_Image * input);
 };
 
 #endif /* GILOPERATION_H_ */

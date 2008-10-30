f108a4a82998c8adf4c718320660490f241e7a84
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-10-30 22:10:11 +0000

    The purpose of ImageBox is to add an event handler to the Fl_Box that is used to display the main image.

---
+++ trunk/src/main/cpp/ImageBox.h
@@ -0,0 +1,43 @@
+/*
+ * ImageBox.h
+ *
+ * The purpose of ImageBox is to add an event handler to the Fl_Box that is
+ * used to display the main image.
+ *
+ *  Created on: Oct 28, 2008
+ *      Author: Sami Badawi
+ */
+
+#ifndef IMAGEBOX_H_
+#define IMAGEBOX_H_
+
+#include <FL/Fl_Box.H>
+#include <ctime>
+
+class Fl_Scroll;
+class Fl_Image;
+class ImageController;
+
+class ImageBox : public Fl_Box {
+	 Fl_Scroll * _scroll;
+	 const char * _brushType;
+
+public:
+	ImageBox(int x, int y, int w, int h, const char *l=0);
+	virtual ~ImageBox();
+
+	Fl_Image * getImage();
+	virtual void setScroll(Fl_Scroll *val);
+	virtual void setBrushType(const char * brushType);
+	virtual int handle(int e);
+
+//Handler methods
+    virtual void pointDraw();
+    virtual void showPixel();
+    virtual void refresh(bool timedependent = false);
+
+    int instanceNo; //TODO take out
+    time_t _timeOfLastRefreshInSeconds;
+};
+
+#endif /* IMAGEBOX_H_ */

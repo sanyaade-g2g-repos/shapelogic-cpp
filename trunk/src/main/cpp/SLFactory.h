7685e322ee929c10b1228a8e0ae1bca675c4369b
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-11-07 18:37:16 +0000

    Added SLFactory class.

---
+++ trunk/src/main/cpp/SLFactory.h
@@ -0,0 +1,46 @@
+/*
+ * SLFactory.h
+ *
+ *  This is the main factory for ShapeLogic C++.
+ *
+ *  This might be split into more specialized factory if the need arise.
+ *
+ *  Created on: Nov 7, 2008
+ *      Author: Sami Badawi
+ */
+
+#ifndef SLFACTORY_H_
+#define SLFACTORY_H_
+
+class SLImage;
+class FltkImage;
+//class OpenCVImage;
+
+class SLFactory {
+public:
+	SLFactory();
+	virtual ~SLFactory();
+
+	//Get the instance used in the GUI
+	static SLFactory * getInstance();
+
+	const char * getImageType () const;
+	void setImageType(const char * imageType);
+
+	SLImage * makeSLImage(const char * filename) const;
+
+	FltkImage * makeFltkImage(const char * filename) const;
+
+//	OpenCVImage * makeOpenCVImage(const char * filename) const;
+
+	SLImage * makeSimilarImage(const SLImage * image) const;
+
+	FltkImage * makeSimilarFltkImage(const SLImage * image) const;
+
+protected:
+	const char * _imageType;
+
+	static SLFactory * INSTANCE;
+};
+
+#endif /* SLFACTORY_H_ */

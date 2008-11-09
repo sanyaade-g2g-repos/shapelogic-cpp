5641376805a4d2e507de23077b56546787b0a8bb
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-11-09 04:22:56 +0000

    Added OpenCVImage. This is now the default image. This has not been tested on Windows yet.

---
+++ trunk/src/main/cpp/OpenCVImage.cpp
@@ -0,0 +1,174 @@
+/*
+ * FltkImage.cpp
+ *
+ *  Created on: Oct 2, 2008
+ *      Author: Sami Badawi
+ */
+
+#include "OpenCVImage.h"
+#include <FL/Fl_Image.H>
+#include <boost/gil/extension/dynamic_image/image_view_factory.hpp> //for interleaved_view()
+#include <opencv/cv.h>
+#include <opencv/cxcore.h>
+#include <opencv/highgui.h>
+
+using namespace boost::gil;
+
+OpenCVImage::OpenCVImage(IplImage * image) : SLImage() {
+	_iplImage = image;
+	_flImage = NULL;
+}
+
+OpenCVImage::~OpenCVImage() {
+	cvReleaseImage(&_iplImage);
+	if (_flImage) {
+		_flImage->uncache();
+//You can set buffer in the constructor but you cannot set it to something new later
+//		*_flImage->data() = NULL;
+//		delete _flImage;
+	}
+}
+
+/** Returns the width of this image in pixels. */
+int OpenCVImage::getWidth() const {
+	if (0 !=_iplImage)
+		return _iplImage->width;
+	return 0;
+}
+
+/** Returns the height of this image in pixels. */
+int OpenCVImage::getHeight() const {
+	if (0 !=_iplImage)
+		return _iplImage->height;
+	return 0;
+}
+
+//void OpenCVImage::setRoi(Rectangle roi) {
+//
+//}
+//
+//void OpenCVImage::setRoi(int x, int y, int rwidth, int rheight) {
+//
+//}
+//
+//Rectangle OpenCVImage::getRoi() {
+//
+//}
+
+int OpenCVImage::getPixelCount() const {
+	return getWidth() * getHeight();
+}
+
+/** Returns a reference to this image's pixel array. The
+array type (byte[], short[], float[] or int[]) varies
+depending on the image type. */
+unsigned char * OpenCVImage::getPixels() const { //TODO should this be replaced by getBuffer()
+	if (0 == _iplImage)
+		return 0;
+	return (unsigned char *) _iplImage->imageData;
+}
+
+/** Sets a new pixel array for the image. The length of the array must be equal to width*height.
+Use setSnapshotPixels(null) to clear the snapshot buffer. */
+void OpenCVImage::setPixels(unsigned char * pixels) {
+
+}
+
+/** Returns true if this image uses an inverting LUT
+that displays zero as white and 255 as black. */
+bool OpenCVImage::isInvertedLut() const {
+    return false;
+}
+
+/** Returns the number of color channels in the image. The color channels can be
+*  accessed by toFloat(channelNumber, fp) and written by setPixels(channelNumber, fp).
+* @return 1 for grayscale images, 3 for RGB images
+*/
+int OpenCVImage::getNChannels() const {
+	if (0 != _iplImage)
+		return _iplImage->nChannels;
+	return 0;
+}
+
+bool OpenCVImage::isEmpty() const {
+	return 0 == _iplImage;
+}
+
+bool OpenCVImage::isGray() const {
+	return false;
+}
+
+bool OpenCVImage::isGray8() const {
+	if (0 !=_iplImage)
+		return 1 == _iplImage->nChannels;
+	return false;
+}
+
+bool OpenCVImage::isGray16() const {
+	return false;
+}
+
+bool OpenCVImage::isRgb() const {
+	if (0 !=_iplImage)
+		return 3 <= _iplImage->nChannels;
+	return true;
+}
+
+//Rectangle OpenCVImage::getActiveRectangle() {
+//
+//}
+
+int OpenCVImage::getLineStride() const {
+    return getWidth();
+}
+
+Fl_Image * OpenCVImage::getFlImage() const {
+	if (NULL ==_flImage) {
+		_flImage = new Fl_RGB_Image(getPixels(),getWidth(), getHeight(),
+				getNChannels(), getWidth() * getNChannels());
+	}
+	return _flImage;
+}
+
+OpenCVImage * OpenCVImage::copy() const {
+	IplImage * copyOfFlImage = cvCloneImage(_iplImage );
+	OpenCVImage * result = new OpenCVImage(copyOfFlImage);
+	result->setFilename(getFilename());
+	return result;
+}
+
+rgb8_view_t OpenCVImage::make_rgb8_view_t() const {
+	rgb8_view_t view =
+		interleaved_view(
+				getWidth(),
+				getHeight(),
+				(rgb8_view_t::value_type *)(getPixels()),
+				getWidth()*getNChannels());
+	return view;
+}
+
+gray8_view_t OpenCVImage::make_gray8_view_t() const {
+	gray8_view_t view =
+		interleaved_view(
+				getWidth(),
+				getHeight(),
+				(gray8_view_t::value_type *)(getPixels()),
+				getWidth());
+	return view;
+}
+
+const char * OpenCVImage::getFilename() const {
+	return _filename.c_str();
+}
+
+void OpenCVImage::setFilename(const char * filename) {
+	_filename = filename;
+}
+
+void OpenCVImage::swapRB() {
+	cvConvertImage( _iplImage, _iplImage,CV_CVTIMG_SWAP_RB );
+}
+
+OpenCVImage * OpenCVImage::makeSimilarImage() const {
+	return copy(); //TODO change to make an empty image
+}
---
+++ trunk/src/main/cpp/OpenCVImage.h
@@ -0,0 +1,107 @@
+/*
+ * FltkImage.h
+ *
+ * The idea with this class is that it should be able to provide images of all
+ * the classes FLTK, GIL and OpenCV.
+ *
+ * To begin with I will attempt to only have one image buffer stored at the
+ * same time.
+ *
+ *  Created on: Oct 2, 2008
+ *      Author: Sami Badawi
+ */
+
+#ifndef OPENCVIMAGE_H_
+#define OPENCVIMAGE_H_
+
+#include "SLImage.h"
+
+#include <boost/gil/typedefs.hpp>
+#include <string>
+
+class Fl_Image;
+struct _IplImage;
+
+class OpenCVImage : public SLImage {
+public:
+	OpenCVImage(_IplImage * _image);
+	virtual ~OpenCVImage();
+
+	/** Returns the width of this image in pixels. */
+	virtual int getWidth() const;
+
+	/** Returns the height of this image in pixels. */
+	virtual int getHeight() const;
+
+//	virtual void setRoi(Rectangle roi) = 0;
+//
+//	virtual void setRoi(int x, int y, int rwidth, int rheight) = 0;
+//
+//	virtual Rectangle getRoi() = 0;
+
+	virtual int getPixelCount() const;
+
+	/** Returns a reference to this image's pixel array. The
+	array type (byte[], short[], float[] or int[]) varies
+	depending on the image type. */
+	virtual unsigned char * getPixels() const;
+
+	/** Sets a new pixel array for the image. The length of the array must be equal to width*height.
+	Use setSnapshotPixels(null) to clear the snapshot buffer. */
+	virtual void setPixels(unsigned char * pixels);
+
+	/** Returns true if this image uses an inverting LUT
+	that displays zero as white and 255 as black. */
+	virtual bool isInvertedLut() const;
+
+	/** Returns the number of color channels in the image. The color channels can be
+	*  accessed by toFloat(channelNumber, fp) and written by setPixels(channelNumber, fp).
+	* @return 1 for gray scale images, 3 for RGB images
+	*/
+	virtual int getNChannels() const;
+
+	virtual bool isEmpty() const;
+
+	virtual bool isGray() const;
+
+	virtual bool isGray8() const;
+
+	virtual bool isGray16() const;
+
+	virtual bool isRgb() const;
+
+//	virtual Rectangle getActiveRectangle();
+
+	virtual int getLineStride() const;
+
+//-------------New members now part of base class SLImage
+
+	virtual Fl_Image * getFlImage() const;
+
+	virtual OpenCVImage * copy() const;
+
+	virtual boost::gil::rgb8_view_t make_rgb8_view_t() const;
+
+	virtual boost::gil::gray8_view_t make_gray8_view_t() const;
+
+	virtual const char * getFilename() const;
+
+	virtual void setFilename(const char * filename);
+
+	virtual OpenCVImage * makeSimilarImage() const;
+
+	//-------------Special for OpenCVImage
+
+	virtual void swapRB();
+
+protected:
+	//Now a secondary image, without separated buffer, should be instantiated lazily
+	mutable Fl_Image * _flImage;
+
+	//Primary image
+	_IplImage * _iplImage;
+
+	std::string _filename;
+};
+
+#endif /* OPENCVIMAGE_H_ */

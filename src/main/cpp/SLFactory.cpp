7cf16e733c20c5efe112d86fa35a90d68d080bcf
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-11-11 00:13:48 +0000

    Took out factory methods for load image that are now in SLImage.

--- src/main/cpp/SLFactory.cpp
+++ src/main/cpp/SLFactory.cpp
@@ -11,15 +11,8 @@
 #include "SLFactory.h"
 #include "SLStringUtil.h"
 
-#include <FL/Fl_Bitmap.H>
-#include <FL/Fl_JPEG_Image.H>
 #include <FL/Fl_Shared_Image.H> // Image I/O
 
-#include <opencv/cv.h>
-#include <opencv/cxcore.h>
-#include <opencv/highgui.h>
-
-#include <cmath> //for OpenCV
 #include <cstring> //strcmp
 
 using namespace std;
@@ -28,7 +21,7 @@ SLFactory * SLFactory::INSTANCE = 0;
 
 SLFactory::SLFactory() {
 	_imageType = "OpenCV";
-	fl_register_images();
+	fl_register_images(); //TODO make conditional
 }
 
 SLFactory::~SLFactory() {
@@ -49,35 +42,8 @@ void SLFactory::setImageType(const char * imageType) {
 
 SLImage * SLFactory::makeSLImage(const char * filename) const {
 	if (0 == strcmp("OpenCV",_imageType))
-		return makeOpenCVImage(filename);
-	return makeFltkImage(filename);
-}
-
-FltkImage * SLFactory::makeFltkImage(const char * filename) const {
-	if (SLStringUtil::empty(filename))
-		return 0;
-	Fl_Shared_Image *sharedImage = Fl_Shared_Image::get(filename);
-	Fl_Image * image = sharedImage->copy();
-	sharedImage->release();
-	if ( image->h() == 0 ) {
-		delete image;
-		return 0;
-	}
-	FltkImage * result = new FltkImage(image);
-	result->setFilename(filename);
-	return result;
-}
-
-OpenCVImage * SLFactory::makeOpenCVImage(const char * filename) const {
-	if (SLStringUtil::empty(filename))
-		return 0;
-	IplImage* img=cvLoadImage(filename);
-	if(0 == img) {
-		return 0;
-	}
-	OpenCVImage * openCVImage = new OpenCVImage(img);
-	openCVImage->swapRB();
-	return openCVImage;
+		return OpenCVImage::NULL_OBJECT->load(filename);
+	return FltkImage::NULL_OBJECT->load(filename);
 }
 
 bool SLFactory::saveImageAs(SLImage * image, const char *filename) {

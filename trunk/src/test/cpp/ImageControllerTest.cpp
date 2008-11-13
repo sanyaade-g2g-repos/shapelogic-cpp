9bcb6151d9465a345b9441a81470710abcaaeade
sami.badawi <sami.badawi@d9d24fb1-fe55-0410-a27a-3f1a13a0f462>
2008-11-13 19:09:21 +0000

    Use OpenCVImage instead of FltkImage, to get it to build under both Windows and UNIX.

--- trunk/src/test/cpp/ImageControllerTest.cpp
+++ trunk/src/test/cpp/ImageControllerTest.cpp
@@ -30,18 +30,18 @@ TEST(ImageControllerTest, loadSLImageWithImageController) {
     EXPECT_EQ(92, image->ld()); //4 byte aligned under OpenCV not under FLTK
 }
 
-// Tests Open SLImage
-TEST(ImageControllerTest, loadSLImageWithSLFactory) {
-	SLFactory sLFactory;
-	sLFactory.setImageType("FLTK");
-	SLImage * slImage = sLFactory.makeSLImage(filename);
-    Fl_Image * image = slImage->getFlImage();
-    ASSERT_NE((void *)NULL, image ) << "File did not open " << filename;
-    EXPECT_EQ(30, image->w());
-    EXPECT_EQ(30, image->h());
-    EXPECT_EQ(3, image->d());
-    EXPECT_EQ(0, image->ld()); //4 byte aligned under OpenCV not under FLTK
-}
+// Tests Open SLImage. Disabled since PNG image in FLTK caused linker problems
+//TEST(ImageControllerTest, loadSLImageWithSLFactory) {
+//	SLFactory sLFactory;
+//	sLFactory.setImageType("FLTK");
+//	SLImage * slImage = sLFactory.makeSLImage(filename);
+//    Fl_Image * image = slImage->getFlImage();
+//    ASSERT_NE((void *)NULL, image ) << "File did not open " << filename;
+//    EXPECT_EQ(30, image->w());
+//    EXPECT_EQ(30, image->h());
+//    EXPECT_EQ(3, image->d());
+//    EXPECT_EQ(0, image->ld()); //4 byte aligned under OpenCV not under FLTK
+//}
 
 // Tests load OpenCVImage
 TEST(ImageControllerTest, loadOpenCVImage) {
@@ -57,16 +57,16 @@ TEST(ImageControllerTest, loadOpenCVImage) {
     EXPECT_EQ(92, openCVImage->getWidthStep());
 }
 
-// Tests load FltkImage
-TEST(ImageControllerTest, loadFltkImage) {
-	FltkImage * fltkImage = FltkImage::NULL_OBJECT->load(filename);
-    ASSERT_NE((void *)NULL, fltkImage ) << "File did not open " << filename;
-    Fl_Image * image = fltkImage->getFlImage();
-    ASSERT_NE((void *)NULL, image ) << "File did not open " << filename;
-    EXPECT_EQ(30, image->w());
-    EXPECT_EQ(30, image->h());
-    EXPECT_EQ(3, image->d());
-    EXPECT_EQ(0, image->ld()); //4 byte aligned under OpenCV not under FLTK
-    EXPECT_EQ(8, fltkImage->getDepth());
-    EXPECT_EQ(90, fltkImage->getWidthStep());
-}
+// Tests load FltkImage. Disabled since PNG image in FLTK caused linker problems
+//TEST(ImageControllerTest, loadFltkImage) {
+//	FltkImage * fltkImage = FltkImage::NULL_OBJECT->load(filename);
+//    ASSERT_NE((void *)NULL, fltkImage ) << "File did not open " << filename;
+//    Fl_Image * image = fltkImage->getFlImage();
+//    ASSERT_NE((void *)NULL, image ) << "File did not open " << filename;
+//    EXPECT_EQ(30, image->w());
+//    EXPECT_EQ(30, image->h());
+//    EXPECT_EQ(3, image->d());
+//    EXPECT_EQ(0, image->ld()); //4 byte aligned under OpenCV not under FLTK
+//    EXPECT_EQ(8, fltkImage->getDepth());
+//    EXPECT_EQ(90, fltkImage->getWidthStep());
+//}
